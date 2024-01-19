c1ce5c99c9c29cb155696f3e2b7f697130abd8ba
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2024-01-19 11:30:03 -0300

    fazendo building

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Consegui iniciar o aplicativo!</Text>
+      <Text>iniciando Aplicativo</Text>
       <StatusBar style="auto" />
     </View>
   );
